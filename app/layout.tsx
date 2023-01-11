402355db2ffe1a71ddb0628a8a7af8692ee1dd59
Pavel Pazderník <pazdernik.pavel@email.cz>
2023-01-11 22:41:51 +0100

    Add twitter metatags

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,22 @@ export default function RootLayout({
           property="og:description"
           content="Jmenuji se Pavel a věnuji se vývoji, především tedy frontendu, již 7 rokem."
         />
+        <meta property="og:site_name" content="pazdy.dev" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https:pazdy.dev" />
         <meta property="og:image" content="/image.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@PavelPazdernik" />
+        <meta
+          name="twitter:title"
+          content="Pavel Pazderník - Software Developer"
+        />
+        <meta
+          name="twitter:description"
+          content="Jmenuji se Pavel a věnuji se vývoji, především tedy frontendu, již 7 rokem."
+        />
+        <meta name="twitter:image" content="/image.jpg" />
+
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://use.typekit.net/wvb5aqj.css" />
       </head>
